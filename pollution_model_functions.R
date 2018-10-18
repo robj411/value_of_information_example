96eca1dcfc491db8f4142504dcd462d53343c3e4
Rob Johnson <rob.johnson87@ntlworld.com>
2018-10-18 13:14:49 +0100

    constant mets added to constants

--- pollution_model_functions.R
+++ pollution_model_functions.R
@@ -67,25 +67,25 @@ get_parameters <- function(){
   parameters$zeta <- Beta(2, 3) # proportion of pollution attributed to traffic
   
   ## POLLUTION AND HEALTH VARIABLES ##
-  parameters$xi5 <- Lnorm(0,0.5) # chronic obstructive pulmonary disease
+  parameters$xi2 <- Lnorm(0,0.5) # stroke
   parameters$xi3 <- Lnorm(0,0.5) # ischemic heart disease
   parameters$xi4 <- Lnorm(0,0.5) # lung cancer
-  parameters$xi2 <- Lnorm(0,0.5) # stroke
+  parameters$xi5 <- Lnorm(0,0.5) # chronic obstructive pulmonary disease
   
-  # ventilation rates for walking and cycling
-  parameters$lambda1 <- Lnorm(1,1)
-  parameters$lambda2 <- Lnorm(2,0.4)
+  # ventilation rates 
+  parameters$lambda1 <- Lnorm(1,1) # walking
+  parameters$lambda2 <- Lnorm(2,0.4) # cycling
+  # (0.5) # bus
+  # (0.5) # car
+  # (1) # motorbike
+  # (0.5) # goods vehicle
+  
   
   # allocation of traffic pollution to modes
   parameters$alpha1 <- Gammad(shape=32,scale=1) # buses
   parameters$alpha2 <- Gammad(shape=8,scale=1) # cars
   parameters$alpha3 <- Gammad(shape=4,scale=1) # motorbikes
   parameters$alpha4 <- Gammad(shape=56,scale=1) # goods vehicles
-  
-  parameters$lambda3 <- Dirac(0.5)
-  parameters$lambda4 <- Dirac(0.5)
-  parameters$lambda5 <- Dirac(1)
-  parameters$lambda6 <- Dirac(0.5)
   
   return(parameters)
 }
@@ -97,38 +97,40 @@ interp <- function(V,HH){
   return(out)
 }
 
-pollution_calculation <- function(const,parameters,seed){
+pollution_calculation <- function(const,parameters){
+  
   ## INDEX TRANSLATIONS
-  set.seed(seed)
+  # which diseases we need from the global burden of disease dataset
   disease_all_in_pm <- c(1,2,16,17)
   
+  # which of the modes are recorded in travel diary
   all_modes_to_travel <- 1:6
+  # which of the modes are active transport
   all_modes_to_active <- 1:2
   
-  lambda_indices <- 1:6
-  
   ## CONSTANTS
-  
   for(i in 1:length(const)) assign(names(const)[i],const[[i]])
   
-  ## PARAMETER VALUES ##
+  ## PARAMETER VALUES
   parameter_samples <- c()
   for(i in 1:length(parameters)) {
     assign(names(parameters)[i],r(parameters[[i]])(1))
     parameter_samples[i] <- get(names(parameters)[i])
   }
   
+  ## PARAMETER GROUPINGS
   # proportion of traffic pollution attributable to each mode
   alpha <- c(alpha1,alpha2,alpha3,alpha4)
   alpha <- alpha/sum(alpha)
   parameter_samples[which(names(parameters)%in%c("alpha1","alpha2","alpha3","alpha4"))] <- alpha
   P <- to.tensor(c(0,0,alpha),dims=c(travel_modes=6),ndimnames=list(dimnames(TTT)[[2]]))
-  # ventilation rate
-  lambda <- to.tensor(1+c(lambda1,lambda2,lambda3,lambda4,lambda5,lambda6),dims=c(travel_modes=6),ndimnames=list(dimnames(TTT)[[2]]))
+  # ventilation rates
+  lambda <- to.tensor(c(lambda1,lambda2,lambda3,lambda4,lambda5,lambda6),dims=c(travel_modes=6),ndimnames=list(dimnames(TTT)[[2]]))
   # disease relative risks
   xi <- to.tensor(c(xi2,xi3,xi4,xi5),dims=c(disease_pm=4),ndimnames=list(dimnames(H)[[1]]))
   
-  ## total population travel = average travel per person time population numbers
+  ## TRAVEL CALCULATIONS
+  # total population travel = average travel per person time population numbers
   A <- TT*N
   Atil <- margin.tensor(A,i=c('age','gender'))/dim(A)['age']/dim(A)['gender']
   Ahat <- Atil/Atil[[scenario=1]]
@@ -136,26 +138,43 @@ pollution_calculation <- function(const,parameters,seed){
   Ahat[is.nan(Ahat)] <- 1
   A[is.nan(A)] <- 1
   
+  ## POLLUTION CALCULATIONS
+  # pollution from individual modes = original pollution from mode * change in mode use
   Ptil <- P*Ahat[[all_modes=~travel_modes,travel_modes=all_modes_to_travel]]
-  Phat <- margin.tensor(Ptil,i='travel_modes') # marginalise over travel mode
-  
+  # total pollution from traffic = sum over modes
+  Phat <- margin.tensor(Ptil,i='travel_modes') 
+  # total pollution = background pollution (eta(1-zeta)) plus new traffic pollution (eta*zeta*Phat)
   Pbar <- eta*(zeta*Phat+1-zeta)
   
-  V <- lambda
-  Vtilnotmet <- V*TTT
-  Vtilnotmet <- margin.tensor(Vtilnotmet,i='travel_modes')##!! marginalise over modes. should be 1:5 (6=tube)
-  Vhat <- (Vtilnotmet + 1440 - margin.tensor(TTT,i='travel_modes'))/1440
+  ## VENTILATION CALCULATIONS
+  # ventilation rate = 1+lambda
+  V <- 1+lambda
+  # ventilation in travel = ventilation rate in mode * time spent in mode
+  Vtil <- V*TTT
+  # total ventilation = sum over modes
+  Vtil <- margin.tensor(Vtil,i='travel_modes')
+  # overall ventilation = travel ventilation + non-travel ventilation
+  Vhat <- (Vtil + 1440 - margin.tensor(TTT,i='travel_modes'))/1440
+  # pollution inhalation = overall ventilation * pollution level
   Vche <- Vhat*Pbar
   
+  ## HEALTH-IMPACT CALCULATIONS
+  # uncertainty in relative-risk dose--response curve
   Htil <- 1+(H-1)*xi
+  # cast as matrix to apply interpolation
   HH <- matrix(Htil,ncol=4,byrow=T)
-  
+  # interpolate relative risk for each disease given pollution inhalation
   Hhat <- apply(Vche,c(2,3),interp,HH=HH)
   Hhat <- as.tensor(Hhat,dims=c(age=6,disease_pm=4,scenario=6,gender=2))
   dimnames(Hhat) <- list(dimnames(U)[[1]],dimnames(H)[[1]],dimnames(TT)[[3]],dimnames(U)[[4]])
+  
+  ## BURDEN-OF-DISEASE CALCULATIONS
+  # relative risk relative to baseline scenario
   Hche <- Hhat/Hhat[[scenario=1]]
+  # burden of disease = burden of disease in baseline scaled by relative risk ratio
   Util <- Hche*U[[disease=~disease_pm,disease_pm=disease_all_in_pm]]
-  return(list(parameter_samples=parameter_samples,Util=Util,Pbar=Pbar,V=V,Hche=Hche,U=U[[disease=~disease_pm,disease_pm=disease_all_in_pm]]))
+  
+  return(list(parameter_samples=parameter_samples,Util=Util))
 }
 
 
