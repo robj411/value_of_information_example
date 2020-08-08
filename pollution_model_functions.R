472f166a90f301c062a5949f70a75f3d8d727a60
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2020-08-08 07:34:03 +0100

    tornado

--- pollution_model_functions.R
+++ pollution_model_functions.R
@@ -24,20 +24,24 @@ dose_response <- function(pm,alpha,beta,gamma,tmrel){
   1 + alpha * ( 1 - exp(- beta * ( pmax(pm - tmrel,0) )^gamma ) )
 }
 
-pollution_calculation <- function(parameters,j){
+pollution_calculation <- function(params,j,full_parameters=NULL){
   
   ## PARAMETER VALUES
   parameter_samples <- c()
-  for(i in 1:length(parameters)) {
-    assign(names(parameters)[i],parameters[[i]][j])
-    parameter_samples[i] <- get(names(parameters)[i])
+  for(i in 1:length(params)) {
+    assign(names(params)[i],params[[i]][j])
+    parameter_samples[i] <- get(names(params)[i])
   }
   
   ## POLLUTION CALCULATION
   scenario_pm <- x1*(x2*scenario_travel_ratio+1-x2)
   
   ## HEALTH-IMPACT CALCULATIONS
-  RR_realisation <- dose_response(scenario_pm,alpha,beta,gamma,tmrel) 
+  if(exists('DR_quantile')){
+    RR_realisation <- sapply(scenario_pm,function(spm)quantile(dose_response(spm,full_parameters[[3]],full_parameters[[4]],full_parameters[[5]],full_parameters[[6]]),DR_quantile ))
+  }else{
+    RR_realisation <- dose_response(scenario_pm,alpha,beta,gamma,tmrel) 
+  }
   
   ## BURDEN-OF-DISEASE CALCULATIONS
   # relative risk relative to baseline scenario
